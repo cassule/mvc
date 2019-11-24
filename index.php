7e7a54cebe350ca412bd639de57b5aa7d5edd416
Josué Francisco <josuefrancisco2017@outlook.pt>
2019-11-25 00:17:13 +0100

    config slim routes

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+
+require_once ("vendor/autoload.php");
+require_once ("init.php");
+
+$app = new \Slim\App([ 'settings' => [
+    'displayErrorDetails' => true
+    ]
+]);
+  
+$app->get('/', function ()
+{
+    $UsersController = new \App\Controllers\UsersController;
+    $UsersController->index();
+});
+ 
+ 
+$app->get('/add', function ()
+{
+    $UsersController = new \App\Controllers\UsersController;
+    $UsersController->create();
+});
+ 
+$app->post('/add', function ()
+{
+    $UsersController = new \App\Controllers\UsersController;
+    $UsersController->store();
+});
+ 
+$app->get('/edit/{id}', function ($request)
+{
+
+    $id = $request->getAttribute('id');
+ 
+    $UsersController = new \App\Controllers\UsersController;
+    $UsersController->edit($id);
+});
+ 
+
+$app->post('/edit', function ()
+{
+    $UsersController = new \App\Controllers\UsersController;
+    $UsersController->update();
+});
+ 
+
+$app->get('/remove/{id}', function ($request)
+{
+    
+    $id = $request->getAttribute('id');
+ 
+    $UsersController = new \App\Controllers\UsersController;
+    $UsersController->remove($id);
+});
+ 
+$app->run();
+?>
--- init.php
+++ init.php
@@ -4,11 +4,10 @@ define('BASE_PATH', dirname(__FILE__));
  
 
 define('MYSQL_HOST', 'localhost');
-define('MYSQL_USER', 'usuario');
-define('MYSQL_PASS', 'senha');
-define('MYSQL_DBNAME', 'nome_do_banco');
+define('MYSQL_USER', 'root');
+define('MYSQL_PASS', '');
+define('MYSQL_DBNAME', 'db_usercontrol');
  
 // php config
 ini_set('display_errors', true);
 error_reporting(E_ALL);
-date_default_timezone_set('Africa/Luanda');
