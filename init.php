254ded41414301edcf4000630adb173ffd1f1098
Josué Francisco <josuefrancisco2017@outlook.pt>
2019-11-25 00:02:37 +0100

    initial commit

---
+++ init.php
@@ -0,0 +1,14 @@
+<?php
+
+define('BASE_PATH', dirname(__FILE__));
+ 
+
+define('MYSQL_HOST', 'localhost');
+define('MYSQL_USER', 'usuario');
+define('MYSQL_PASS', 'senha');
+define('MYSQL_DBNAME', 'nome_do_banco');
+ 
+// php config
+ini_set('display_errors', true);
+error_reporting(E_ALL);
+date_default_timezone_set('Africa/Luanda');
